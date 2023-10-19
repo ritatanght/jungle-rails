9909884cc73e2c1c070bd8d84c12b732c96adff6
Rita Tang <doggymint@gmail.com>
2023-10-18 20:35:08 -0400

    Revise routes for the sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  get 'users/create'
   root to: 'products#index'
-
+  
   get '/about' => 'about#index'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
