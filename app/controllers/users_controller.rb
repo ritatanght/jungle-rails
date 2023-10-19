51cbdc01bd7f258671041aed58370076966cd112
Rita Tang <doggymint@gmail.com>
2023-10-19 11:56:20 -0400

    Fix variable name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to root_path
 
     else
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(
       :first_name,
       :last_name,
+      :email,
       :password,
       :password_confirmation
     )
