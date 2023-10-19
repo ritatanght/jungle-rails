b491f35f210642faa30de56d8c498529c7ef9bb5
Rita Tang <doggymint@gmail.com>
2023-10-18 20:48:28 -0400

    Complete logic for creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,26 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = user.id
+      redirect_to root_path
+
+    else
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :password,
+      :password_confirmation
+    )
+  end
+
 end
