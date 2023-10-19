8d8495db57d0584a76dbfd328e536b6b21916290
Rita Tang <doggymint@gmail.com>
2023-10-18 21:07:58 -0400

    Revise routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'products#index'
   
   get '/about' => 'about#index'
   
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
