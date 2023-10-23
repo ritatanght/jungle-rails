43709f4a830cf0ba772a1e961c45e7d50250aca3
Rita Tang <doggymint@gmail.com>
2023-10-23 15:17:40 -0400

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   
   has_secure_password
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
   
 end
 
