2371f86a7a123d2892792fbdd133b9b37f438ef0
Rita Tang <doggymint@gmail.com>
2023-10-23 15:21:11 -0400

    Add length constraint on password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: {minimum: 4}
   validates :password_confirmation, presence: true
   
 end
