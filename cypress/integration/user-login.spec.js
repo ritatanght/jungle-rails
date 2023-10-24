295b772bf208da008715bde2338f98987feb263a
Rita Tang <doggymint@gmail.com>
2023-10-24 18:55:41 -0400

    Add test for user login

---
+++ cypress/integration/user-login.spec.js
@@ -0,0 +1,14 @@
+describe("User login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("successful login will take the user to the home page", () => {
+    cy.contains("Sign in").click();
+    cy.get("input[name=email]").type("bb@example.com");
+    cy.get("input[name=password]").type("1234{enter}");
+
+    cy.get(".products article").should("have.length", 12);
+    cy.contains("Signed in as Berry").should("be.visible");
+  });
+});
