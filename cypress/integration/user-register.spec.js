17c8dd3d910eba5e86fc401e2c9613abe790a7ff
Rita Tang <doggymint@gmail.com>
2023-10-24 18:58:23 -0400

    Add test for user registration

---
+++ cypress/integration/user-register.spec.js
@@ -0,0 +1,18 @@
+describe("User login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("successful user registration will redirect to home page signed in as the new user", () => {
+    cy.contains("Sign in").click();
+    cy.contains("Sign up").click();
+    cy.get("[id$=first_name]").type("Sherry");
+    cy.get("[id$=last_name]").type("Howe");
+    cy.get("[id$=email]").type("sh@example.com");
+    cy.get("[id$=password]").type("1234");
+    cy.get("[id$=password_confirmation]").type("1234{enter}");
+
+    cy.get(".products article").should("have.length", 12);
+    cy.contains("Signed in as Sherry").should("be.visible");
+  });
+});
