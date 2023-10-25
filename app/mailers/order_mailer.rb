53dc8585d1466fabac28c97377597bd8ce20f733
Rita Tang <doggymint@gmail.com>
2023-10-25 19:04:19 -0400

    Add method to send email for order

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,11 @@
+class OrderMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+   def order_receipt_email
+    @order = params[:order]
+    puts @order
+    @email = @order.email
+    @url  = "http://localhost:3000/orders/#{@order.id}"
+    mail(to: @email, subject: 'Thank you for your order at Jungle')
+  end
+end
