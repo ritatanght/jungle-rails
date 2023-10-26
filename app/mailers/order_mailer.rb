df5d16c6bef858e62283a372cf98e2a87b0b54d2
Rita Tang <doggymint@gmail.com>
2023-10-25 20:52:18 -0400

    Remove url variable

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,9 +3,7 @@ class OrderMailer < ApplicationMailer
 
    def order_receipt_email
     @order = params[:order]
-    puts @order
     @email = @order.email
-    @url  = "http://localhost:3000/orders/#{@order.id}"
     mail(to: @email, subject: "Thank you for your order##{@order.id} at Jungle")
   end
 end
